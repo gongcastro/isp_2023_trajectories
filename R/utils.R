53cbc0335547dbbddaf4077d76de55dd986e8268
gongcastro <gonzalo.garciadecastro@upf.edu>
2023-05-23 02:26:18 +0200

    Test

--- R/utils.R
+++
@@ -1,8 +0,0 @@
-#' Rescale standardised variable
-#'
-#' @param x Numeric vector to be rescaled
-#' @param mean Numeric value indicating the mean of the original vector
-#' @param sd Numeric value indicating the standard deviation of the original vector
-rescale_variable <- function(x, mean, sd) {
-    (x * sd) + mean
-}
